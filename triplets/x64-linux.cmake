d56f632c158e4a2791fa8a31566f50144d6d4104
Damien Charveriat <damien.charveriat@gmail.com>
2022-11-14 14:15:50 +0000

    triplet fix for glog/ceres

--- triplets/x64-linux.cmake
+++ triplets/x64-linux.cmake
@@ -1,6 +1,12 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
+if (${PORT} MATCHES "ceres|glog")
+  set(VCPKG_CRT_LINKAGE dynamic)
+  set(VCPKG_LIBRARY_LINKAGE dynamic)
+else()
+  set(VCPKG_CRT_LINKAGE dynamic)
+  set(VCPKG_LIBRARY_LINKAGE static)
+endif()
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
