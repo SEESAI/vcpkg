5dc8ad96e0777926bd4c3df0601c4b57180702f4
wsolichin-sees <35688386+wsolichin-sees@users.noreply.github.com>
2025-08-05 11:57:31 +0100

    Adding rerun-sdk to 2024-05-28-dev sees.ai branch (#3)
    Updating thrift version for rerun-sdk
    
    Updating rerun SDK to 0.23.2
    
    Update SHA512
    
    Remove rerun-sdk patches for rerun-sdk 0.23.2
    
    Updating rerun-sdk to 0.23.4

---
+++ ports/rerun-sdk/portfile.cmake
@@ -0,0 +1,48 @@
+# Must download SDK distfile because some binaries are prebuilt from Rust.
+vcpkg_download_distfile(
+    ARCHIVE
+    URLS "https://github.com/rerun-io/rerun/releases/download/${VERSION}/rerun_cpp_sdk.zip"
+    FILENAME rerun_cpp_sdk.zip
+    SHA512 a48788475d75b972cff174a095572554e0d85e391c205f2b85bc9f37d67bea63ed11c2a176353fbbff9a1ad9035f645d34248695ce7f626525b432365c571aaf
+)
+
+vcpkg_extract_source_archive(
+    SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DRERUN_DOWNLOAD_AND_BUILD_ARROW=OFF # Disable downloading and building Arrow
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME rerun_sdk CONFIG_PATH "lib/cmake/rerun_sdk")
+
+file(GLOB LIBRERUN_C_FILE
+    RELATIVE "${CURRENT_PACKAGES_DIR}/lib"
+    "${CURRENT_PACKAGES_DIR}/lib/${VCPKG_TARGET_STATIC_LIBRARY_PREFIX}rerunc_c_-*${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX}"
+)
+
+vcpkg_replace_string(
+    "${CURRENT_PACKAGES_DIR}/share/rerun_sdk/rerun_sdkConfig.cmake"
+    "set(RERUN_LIB_DIR \"\${CMAKE_CURRENT_LIST_DIR}/../..\")"
+    "set(RERUN_LIB_DIR \"\${CMAKE_CURRENT_LIST_DIR}/../../lib\")"
+)
+
+vcpkg_replace_string(
+    "${CURRENT_PACKAGES_DIR}/share/rerun_sdk/rerun_sdkConfig.cmake"
+    "${SOURCE_PATH}/lib/${LIBRERUN_C_FILE}"
+    "\${CMAKE_CURRENT_LIST_DIR}/../../lib/${LIBRERUN_C_FILE}"
+)
+
+vcpkg_install_copyright(FILE_LIST
+    "${SOURCE_PATH}/LICENSE-MIT"
+    "${SOURCE_PATH}/LICENSE-APACHE"
+)
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
